07196cf9527e38323d716b80d922c5b15fd0e1b1
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-08-27 15:51:27 -0300

    Refactor service worker cache management

--- sw.js
+++ sw.js
@@ -1,28 +1,10 @@
-const CACHE = "custo-pote-v1";
+const CACHE_NAME = 'caponata-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-const ASSETS = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
