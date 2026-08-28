ed39f16e3ebf8faf2fb07d2a168169763f8dcf32
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-08-28 08:25:09 -0300

    Update service worker caching logic and assets

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
-const CACHE_NAME = 'caponata-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = "custo-pote-v1";
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
 });
